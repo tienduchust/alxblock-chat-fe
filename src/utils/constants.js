62556b9a592163bf66942b0f1345205f44c0afc2
TienDuc <ductt.it.hust@gmail.com>
2025-04-17 09:03:23 +0700

    add env

---
+++ src/utils/constants.js
@@ -0,0 +1,2 @@
+export const MQTT_HOST = process.env.REACT_APP_MQTT_HOST;
+export const MQTT_CLIENT_ID = process.env.REACT_APP_MQTT_CLIENT_ID;
--- src/utils/contants.js
+++
@@ -1,2 +0,0 @@
-export const MQTT_HOST = "wss://test.mosquitto.org:8081";
-export const MQTT_CLIENT_ID = `client-alxblock-${Math.random().toString(16).substr(2, 8)}`;
