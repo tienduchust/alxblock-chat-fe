67cffdeaaf775d2c5e5452e2b6c1738577d67024
TienDuc <ductt.it.hust@gmail.com>
2025-04-17 09:27:53 +0700

    fix client id

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
 export const MQTT_HOST = import.meta.env.VITE_MQTT_HOST;
-export const MQTT_CLIENT_ID = import.meta.env.VITE_MQTT_CLIENT_ID;
+export const MQTT_CLIENT_ID = `client-alxblock-${Math.random().toString(16).substr(2, 8)}`;
